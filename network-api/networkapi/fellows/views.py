f6f545c431a16050d00d5048c059df24b0264e31
Mavis Ou <mmmavis@users.noreply.github.com>
2018-01-17 10:41:30 -0800

    added more content

--- network-api/networkapi/fellows/views.py
+++ network-api/networkapi/fellows/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def fellows_home(request):
-    return render(request, 'fellows_home.html', { 'showHeroBanner': True })
+    return render(request, 'fellows_home.html', {'showHeroBanner': True})
