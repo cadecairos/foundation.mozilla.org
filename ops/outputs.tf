92fe422eb0e7469199461909f795c10c4e9ba6cc
Christopher DeCairos <cade@mozillafoundation.org>
2018-02-09 17:26:12 -0500

    F

--- ops/outputs.tf
+++ ops/outputs.tf
@@ -1,5 +1,5 @@
 output "heroku_git_url" {
-  value       = "git@heroku.com/${heroku_app.django_server.id}.git"
+  value       = "git@heroku.com:${heroku_app.django_server.id}.git"
   description = "The Git endpoint of this heroku app"
   depends_on  = ["heroku_app.django_server"]
 }
