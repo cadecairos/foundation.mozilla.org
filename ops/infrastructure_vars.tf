11e277baab00465f7cdfdc264a1b893af0f7df81
Christopher DeCairos <cade@mozillafoundation.org>
2018-02-09 17:12:45 -0500

    f

--- ops/infrastructure_vars.tf
+++ ops/infrastructure_vars.tf
@@ -35,38 +35,3 @@ variable "personal" {
   type        = "string"
   description = "is this a personal app?"
 }
-
-variable "state_s3_bucket" {
-  type        = "string"
-  description = "Bucket to store terraform state in"
-}
-
-variable "state_s3_key" {
-  type        = "string"
-  description = "key name for the state object"
-}
-
-variable "state_s3_region" {
-  type        = "string"
-  description = "region the S3 bucket is in"
-}
-
-variable "state_encrypt" {
-  type        = "string"
-  description = "Boolean indicating whether or not to use server side encryption"
-}
-
-variable "state_kms_key_id" {
-  type        = "string"
-  description = "The id of the KMS key used for encryption"
-}
-
-variable "state_access_key" {
-  type        = "string"
-  description = "AWS access key"
-}
-
-variable "state_secret_key" {
-  type        = "string"
-  description = "AWS secret access key"
-}
