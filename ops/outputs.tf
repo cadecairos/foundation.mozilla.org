ab4990a59b668def7031d4aad253204c4e454217
Christopher DeCairos <cade@mozillafoundation.org>
2018-02-09 16:38:02 -0500

    depends on burns me again

--- ops/outputs.tf
+++ ops/outputs.tf
@@ -1,4 +1,5 @@
 output "heroku_git_url" {
-  value = "${heroku_app.django_server.git_url}"
+  value       = "${heroku_app.django_server.git_url}"
   description = "The Git endpoint of this heroku app"
+  depends_on  = ["heroku_app.django_server"]
 }
